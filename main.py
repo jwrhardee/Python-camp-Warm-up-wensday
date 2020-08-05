e7ad504cd56d046e9f47ea5a66fbc430a1c648d3
jesseHardee <jwrhardee@gmail.com>
2020-08-05 19:26:00 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+#Variable something that can hold a single piece of data
+# boolean varialbls true and false variables 
+#state variables
+
+# Loops - reapts code
+
+# for Loop
+#reapts code for a given amount of times
+
+# while loop
+#repeats code until its codition is met aka truns 
+count = 0
+while count < 3: 
+  print ("yippy kai yay",count) 
+  count += 1 
+
+
+
